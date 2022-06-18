6e848cf4c27d0e633014a309e0af59131ceaba10
Austin Teng <61093993+austinhteng@users.noreply.github.com>
2022-06-18 16:38:50 -0500

    Delete config.js

--- Tweet-Reader/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-        consumer_key:       'eax9ceSM13u1KMnHAU3vcr4kT'
-    ,   consumer_secret:    'MGfj6XOS3b5KIz9VFDfLfzvXivn7g743ZTlW5wwgulk4Il3eng'
-    ,   client_secret:      'bNxWndCPM7p5hBf8cNxelBcpNGlbxb4vYddBGIT-qtq7LUp8tk'
-}
